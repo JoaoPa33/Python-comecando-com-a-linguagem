43c96702f0649f95ed7a6a2031f48a72842b6035
Joao Paulo Oliveira <medvet.joaop@gmail.com>
2023-03-25 16:13:34 -0300

    03.Testando valores

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,7 +6,17 @@ numero_secreto = 42
 
 chute = int(input("Digite um numero:"))
 # todo input retorna uma string
+
+certo = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
 if (chute == numero_secreto):
-    input("vc acertou")
+    print("Vc acertou")
 else:
-    input("vc errou")
+    if (chute > numero_secreto):
+        print("Você errou: O número é menor do que o digitado")
+    elif (chute < numero_secreto):
+        print("Você errou: O número é maior do que o digitado")
+
+print("fim de jogo")
