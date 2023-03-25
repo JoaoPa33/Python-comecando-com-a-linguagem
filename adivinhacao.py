9d57fb78ce0a1d666098ea2f3c8a6be12a8c2a47
Joao Paulo Oliveira <medvet.joaop@gmail.com>
2023-03-25 15:58:01 -0300

    ADD arquivo python

---
+++ adivinhacao.py
@@ -0,0 +1,12 @@
+print("*"*40)
+print("bem vindo")
+print("*"*40)
+
+numero_secreto = 42
+
+chute = int(input("Digite um numero:"))
+# todo input retorna uma string
+if (chute == numero_secreto):
+    input("vc acertou")
+else:
+    input("vc errou")
