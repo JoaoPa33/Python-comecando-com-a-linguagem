d3f07169973dec2179e6d273afe44eab6c749124
Joao Paulo Oliveira <medvet.joaop@gmail.com>
2023-03-27 15:23:56 -0300

    Inserando a interação e o loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,16 +9,22 @@ total_de_tentativas = 3
 for rodada in range(1, total_de_tentativas):
 # while(rodada <= total_de_tentativas):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas + 1))
-    chute = int(input("Digite um numero:"))
+    chute = int(input("Digite um numero entre 0 e 100:"))
     # todo input retorna uma string
     print("Você digitou:", chute)
 
+    if(chute < 1 or chute > 100 ):
+        print("o numero que vc digitou um número que não esta entre 0 e 100")
+        continue
+# o continue faz com que o codigo pare e volte para cima
     certo = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (chute == numero_secreto):
         print("Vc acertou")
+        break
+# o break faz com que o codigo pare aqui e saia do "for rodada in range(1, total_de_tentativas):"
     else:
         if (chute > numero_secreto):
             print("Você errou: O número é menor do que o digitado")
