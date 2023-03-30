c14f8ef978a29c7ffb46a54a27b3d76eb48b220e
Joao Paulo Oliveira <medvet.joaop@gmail.com>
2023-03-30 10:13:31 -0300

    Adicionando nível ao jogo, definindo uma pontuaçao para o usuario

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,9 +5,19 @@ print("bem vindo")
 print("*"*40)
 
 numero_secreto = random.randrange(0, 101)
-
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 100
 #rodada = 1
+
+print("Qual nível vc gostaria de selecionar?")
+nivel = int(input("(1) facil, (2) médio, (3)dificil:"))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
 # while(rodada <= total_de_tentativas):
@@ -26,6 +36,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (chute == numero_secreto):
         print("Vc acertou")
+        print("Você fez {} pontos".format(pontos))
         break
 # o break faz com que o codigo pare aqui e saia do "for rodada in range(1, total_de_tentativas):"
     else:
@@ -33,7 +44,10 @@ for rodada in range(1, total_de_tentativas + 1):
             print("Você errou: O número é menor do que o digitado")
         elif (chute < numero_secreto):
             print("Você errou: O número é maior do que o digitado")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
     # a funçao for ja incremente, nao precisa do "rodada = rodada + 1"
     print("_"*20)
 print("fim de jogo")
+
